2110c9e154470dbfcc85004faf01dda8b1fcd2d7
secretpray <Renovatio100>
2020-12-15 08:09:29 +0200

    Add delete & edit to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,18 @@
 class PostsController < ApplicationController
 
+   before_action :find_post, only: %i[show edit create update destroy]
+
   def index
     @posts = Post.all.order('created_at DESC')
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+  def show; end
 
   def new
     @post = Post.new
   end
+
+  def edit; end
 
   def create
     @post = Post.new(post_params)
@@ -21,10 +23,28 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Post succefully updated' #t('.success', post: @test.title)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+
+    redirect_to root_path, notice: 'Post succefully deleted'
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:title, :body)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 
 end
