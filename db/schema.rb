901e53d0e32a0b5fc40d783bd6539035b700fa2d
secretpray <Renovatio100>
2020-12-15 08:58:52 +0200

    Add comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_14_181156) do
+ActiveRecord::Schema.define(version: 2020_12_15_061104) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "body", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
@@ -19,4 +28,5 @@ ActiveRecord::Schema.define(version: 2020_12_14_181156) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts"
 end
